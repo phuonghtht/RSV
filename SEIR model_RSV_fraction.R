9d6d7fb1dc57b93bfd441274bd290d68584cfeff
phuong <huynhphuong.iu@gmail.com>
2020-07-14 09:18:42 +0200

    Fitting the data of Australia
    Not fitted as the published figure ?!

--- SEIR model_RSV_fraction.R
+++ SEIR model_RSV_fraction.R
@@ -4,17 +4,18 @@
 
 setwd("/home/phuong/phuonght/gitKraken_project/RSV")
 library(deSolve)
+library(ggplot2)
 
 ######################################### #
 # MODEL SETTINGS                     ----
 ######################################### #
-pop_size          <- 10000000
+pop_size          <- 2200000
 num_days          <- 500
-num_weeks         <- 52*8 # belgium data : 2012- 2018 * 2011 data is incomplete?
-num_days_infected <- 10   #[ 8-11], 6.7 is original estimate
-num_days_exposed  <- 4    # [2,6]
+num_weeks         <- 52*6 
+num_days_infected <- 10  #[ 8-11], 6.7 is original estimate
+num_days_exposed  <- 4   # [2,6]
 num_days_waning   <- 160  # [148, 164] best fit 
-infected_seeds    <- 1 # ? 1= 1000
+infected_seeds    <- 5# ? 1= 1000
 
 ######################################### #
 # INITIALIZE PARAMETERS AND POPULATION ----
@@ -23,38 +24,41 @@ infected_seeds    <- 1 # ? 1= 1000
 
 #  population states
 # S2 <- 0.9 # change this initial stage do change the pattern of peaks
-S1 <- 0.1
-E1 <- infected_seeds/pop_size
+S1 <- 2/80
+E1 <- infected_seeds/(pop_size*S1) 
 I1 <- 0
 R1 <- 0
 
-E2 <- infected_seeds/(2*pop_size)#
+E2 <- 0
 I2 <- 0
 R2 <- 0
-S2 <- 1 - S1- E1- E2
+S2 <- 1-S1-E1-I1-R1-E2-I2-R2 
 
 ######################################### #
 # SET FUNCTION PARAMETERS            ----
 ######################################### #
 # set time frame
-times      <- seq(26, num_weeks, by = 1)
+times      <- seq(1, num_weeks, by = 1)
 
 # set initial health states
 states     <- c(S1 = S1,E1 = E1, I1 = I1, R1 = R1,
                 S2 = S2,E2 = E2, I2 = I2, R2 = R2)
 
 # set parameters
-params     <- c(sigma = 7/ num_days_exposed, #rate of movement from latent to infectious stage
-                gamma = 7/num_days_infected, # recovery rate
-                nu=  7/ num_days_waning,     # rate of loss of immunity
+params     <- c(sigma = 1/0.57, #rate of movement from latent to infectious stage
+                gamma = 1/1.4, # recovery rate
+                # sigma = 7/ num_days_exposed, #rate of movement from latent to infectious stage
+                # gamma = 7/num_days_infected, # recovery rate
+                nu= 0.044,     # rate of loss of immunity
+                # nu=  7/ num_days_waning,     # rate of loss of immunity
                 eta1 = 1/(2*52),             # aging rate 
                 eta2 = 1/(78*52),            # aging rate 
-                eta = 1/(80*52),             # death rate in general
+                # eta = 1/(80*52),             # death rate in general
                 mu = 1/(80*52),              # birth rate
                 beta1 = 0.65,                # the degree of seasonality, range [0,1],higher value stronger seasonal drivers
                 phi = 2.43,                  # phase shift?
                 beta0 = 1.99,                #average transmission rate
-                R0 = 3,                      # reproduction number
+                # R0 = 3,                      # reproduction number
                 delta = 0.65,                # scaled susceptibility
                 alpha = 0.65                 # scaled infectiousness
 )  
@@ -75,7 +79,7 @@ sirv_func <- function(t, states, params) {
        {
          
          # define variables
-         P <- (S1+E1+I1+R1+S2+E2+I2+R2)
+         # P <- (S1+E1+I1+R1+S2+E2+I2+R2)
          beta <- beta0*(1+beta1*cos(2*pi*t/52+phi))
          
          
@@ -100,7 +104,7 @@ sirv_func <- function(t, states, params) {
 ######################################### #
 # use the 'ode' function of deSolve package with our SIR function, health states and parameters
 out <- ode(func = sirv_func, y = states, times = times, parms = params)
-# plot(out)
+plot(out)
 # summary(out)
 
 # par(mfrow = c(1,1))
@@ -121,20 +125,28 @@ out$R <- out[,"R1"] + out[,"R2"]
 
 # Total population
 pop <- out[,"S1"] + out[,"E1"] + out[,"I1"] + out[,"R1"]+out[,"S2"] + out[,"E2"] + out[,"I2"] + out[,"R2"]
-pop <- out[,"S"] + out[,"E"] + out[,"I"] + out[,"R"]
+# pop <- out[,"S1"] + out[,"E1"] + out[,"I1"] + out[,"R1"]
+# pop <- out[,"S"] + out[,"E"] + out[,"I"] + out[,"R"]
+
 # weekly incidence
-# inc <- params["report"]*params["gamma"]*out[,"E"]
-inc <- params["sigma"]*out[,"E"]
-# make a new panel
-par(mfrow=c(1,2))
-# more plots
+inc <- out[,"I1"]
+inc2 <- out[,"I2"]
+inc <- inc*pop_size
+inc2<- inc2*pop_size
+
 time <- out[,"time"]
-plot(time,pop,type='l',lwd=3,ylim = c(0,2))
-plot(time,inc,type='l',lwd=3)
+# plot(time,pop,type='l',lwd=3,ylim = c(0,2))# checking the consistence of pop 
+
+# Plot I1 (<2yrs) and I2(>2yrs)
+par(mfrow=c(1,2))
+plot(time,inc,type='l',lwd=3, col = 1)
+plot(time,inc2,type='l',lwd=3, col = 1)
+
+
 
 # par(mfrow = c(1,1))
-# matplot(out[,1], out[,10:13], type = "l", xlab = "time", ylab = "population fraction")
-# legend("topright", col = 1:8, lty = 1:8, legend = c("S", "E", "I","R"))
+# matplot(out[,1], out[,2:5], type = "l", xlab = "time", ylab = "population fraction")
+# legend("topright", col = 1:4, lty = 1:4, legend = c(colnames(out)[2:5]))
 
 # # plot susceptible class
 # par(mfrow = c(1,1))
@@ -147,13 +159,35 @@ plot(time,inc,type='l',lwd=3)
 # lines(out$R, col=3)
 # lines(out$I, col=2)
 # lines(out$E, col=1)
-# 
+#
 # # add legend
-# legend(x      = 'top', 
-#        legend = c('Susceptible','Infectious','Recovered',"Exposed"), 
-#        col    = c(4,2,3,1), 
+# legend(x      = 'top',
+#        legend = c('Susceptible','Infectious','Recovered',"Exposed"),
+#        col    = c(4,2,3,1),
 #        lwd    = 1,
 #        ncol   = 3,
 #        cex    = 0.9)
 # 
 
+#COMPARING THE MODEL OUTPUT WITH DATA--------------
+aus_data <- read.csv ("./RSV data/Australia_RSV_2yrs_Hannah Moore.csv")
+# metropolitan: 15830 samples ( <- 2yrs, 2000-2005), possitive 21% ( n= 3394)
+# Western Austrilia : 4920 RSV  (<- 2yrs, 2000-2005) positive
+
+# ensuring the total number of cases over the 6 years of
+## the study and the model agreed, ### 3394 cases less than 2yrs
+inc <- inc*pop_size/(sum(inc*pop_size)/4920)
+sum_inc <- sum(inc)
+sum_inc
+
+par(mfrow=c(1,1))
+# label axes
+plot(time,inc,type='l',lwd=3,main = "Predicted RSV",xlab = "Time in weeks",ylab="New reported cases per week",ylim=c(0,max(aus_data[,"Cases"],inc)))
+# plot the data with the model output
+points(aus_data[,"Week"],aus_data[,"Cases"],pch=19,col='red')
+
+#BELGIUM DATA-----------------
+bel_data <- read.csv("./RSV data/RSV_cases_time_epistat.csv")
+bel_data$week <- seq(1,dim(bel_data)[1])
+ggplot(bel_data, aes(week, cases))+
+  geom_line()
