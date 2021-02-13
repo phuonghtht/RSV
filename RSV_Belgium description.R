72c4efabc4c557ca56f1dfe5f1c969f2f01fb05c
phuong <huynhphuong.iu@gmail.com>
2021-02-13 17:27:34 +0100

    Description of RSV incidence in Belgium
    data from Sciensano

--- RSV_Belgium description.R
+++ RSV_Belgium description.R
@@ -6,45 +6,46 @@ library(ggplot2)
 library(scales)
 
 #BELGIUM DATA-----------------
-bel_data <- read.csv("./RSV data/RSV_cases_time_epistat.csv")
-str(bel_data)
-bel_data$date <- as.Date(bel_data$date, format = "%Y-%m-%d")
+case_data <- read.csv("./RSV data/RSV_cases_time_epistat.csv")
+age_data <- read.csv("./RSV data/RSV_cases_age_epistat.csv")
+str(case_data)
+case_data$date <- as.Date(case_data$date, format = "%Y-%m-%d")
 
 #Reported cases 2011-2018=====
-# total cases: 63301 , less than 2yrs: 56126, 1yr:42375,2yrs:13751
-pro_case_less2yrs <- 56126/63301
-pro_case_2yrs <- 13751/63301
-pro_case_1yr <- 42375/63301
+# total cases: 63301 , less than 2yrs: 56126,<1yr(=0):42375,<2yrs(=1):13751, 
+#ef_data_age$cases[ref_data_age$age==0] / sum(ref_data_age$cases)
+pro_case_less2yrs <- sum(age_data$cases[age_data$age<=1]) / sum(age_data$cases)
+pro_case_less1yr <- age_data$cases[age_data$age==0]/ sum(age_data$cases)
 
-bel_data$week <- seq(1,dim(bel_data)[1])
-bel_data$week.yr <- as.numeric(strftime(bel_data$date, format = "%V"))# get week numbers from date
-bel_data$year <- as.numeric(format(bel_data$date,"%Y")) # get year numbers from date
+case_data$week <- seq(1,dim(case_data)[1])
+case_data$week.yr <- as.numeric(strftime(case_data$date, format = "%V"))# get week numbers from date
+case_data$year <- as.numeric(format(case_data$date,"%Y")) # get year numbers from date
 #reassign the year of week 53
-bel_data$year[bel_data$week.yr==53] <- 2015 # start week:"2016-01-02"
+case_data$year[case_data$week.yr==53] <- 2015 # start week:"2016-01-02"
 
-bel_data$epi.period[bel_data$week.yr < 36] = paste0(bel_data$year[bel_data$week.yr <36]-1,"-",bel_data$year[bel_data$week.yr  <36])
-bel_data$epi.period[bel_data$week.yr >= 36] = paste0(bel_data$year[bel_data$week.yr >=36 ],"-",bel_data$year[bel_data$week.yr >=36 ]+1)
-bel_data$epi.period <- as.factor(bel_data$epi.period)
+case_data$epi.period[case_data$week.yr < 36] = paste0(case_data$year[case_data$week.yr <36]-1,"-",case_data$year[case_data$week.yr  <36])
+case_data$epi.period[case_data$week.yr >= 36] = paste0(case_data$year[case_data$week.yr >=36 ],"-",case_data$year[case_data$week.yr >=36 ]+1)
+case_data$epi.period <- as.factor(case_data$epi.period)
 # week.yr = 36 ~ week.epi.period = 1 
-bel_data$week.epi.period[bel_data$epi.period=="2010-2011"] <- c(18:52)# nodat in 2010
-bel_data$week.epi.period[bel_data$epi.period=="2011-2012"] <- c(1:52)
-bel_data$week.epi.period[bel_data$epi.period=="2012-2013"] <- c(1:52)
-bel_data$week.epi.period[bel_data$epi.period=="2013-2014"] <- c(1:52)
-bel_data$week.epi.period[bel_data$epi.period=="2014-2015"] <- c(1:52)
-bel_data$week.epi.period[bel_data$epi.period=="2015-2016"] <- c(1:52)
-bel_data$week.epi.period[bel_data$epi.period=="2016-2017"] <- c(1:53)#53 wks
-bel_data$week.epi.period[bel_data$epi.period=="2017-2018"] <- c(1:30)# wk13 2018
+case_data$week.epi.period[case_data$epi.period=="2010-2011"] <- c(18:52)# nodat in 2010
+case_data$week.epi.period[case_data$epi.period=="2011-2012"] <- c(1:52)
+case_data$week.epi.period[case_data$epi.period=="2012-2013"] <- c(1:52)
+case_data$week.epi.period[case_data$epi.period=="2013-2014"] <- c(1:52)
+case_data$week.epi.period[case_data$epi.period=="2014-2015"] <- c(1:52)
+case_data$week.epi.period[case_data$epi.period=="2015-2016"] <- c(1:52)
+case_data$week.epi.period[case_data$epi.period=="2016-2017"] <- c(1:53)#53 wks
+case_data$week.epi.period[case_data$epi.period=="2017-2018"] <- c(1:30)# wk13 2018
 
 
 
-# bel_data$cases.less2yrs <- round(bel_data$cases*pro_case_less2yrs)
-bel_data$cases.1yr <- round(bel_data$cases*pro_case_1yr)
-bel_data$cases.2yrs <- round(bel_data$cases*pro_case_2yrs)
+# case_data$cases.less2yrs <- round(case_data$cases*pro_case_less2yrs)
+case_data$cases.1yr <- round(case_data$cases*pro_case_1yr)
+case_data$cases.2yrs <- round(case_data$cases*pro_case_2yrs)
 
 break.vec <- c(as.Date("2011-01-08"),
                seq(from=as.Date("2011-01-08"), to=as.Date("2018-03-31"),
                    by="16 weeks"))
-ggplot(bel_data,aes(x = date, y= cases))+
+ggplot(case_data,aes(x = date, y= cases))+
          geom_line(color ="steelblue")+
   # scale_color_manual(name = "", values = c("red","blue","black"))+
   # ggtitle (" Weekly RSV reported in Belgium")+
@@ -58,7 +59,7 @@ ggplot(bel_data,aes(x = date, y= cases))+
         axis.text.y = element_text(size = 14, face = "bold"))
 
 #Reported cases every year=====
-ggplot(bel_data, aes(x = week.epi.period)) + 
+ggplot(case_data, aes(x = week.epi.period)) + 
   geom_line(aes(y = cases, fill=epi.period, color= epi.period)) +
   scale_x_continuous(breaks = c(1,5,9,13,17,21,25,29,33,37,41,45,49),
                    labels = c(36,40,44,48,52,4,8,12,16,20,24,28,32))+
